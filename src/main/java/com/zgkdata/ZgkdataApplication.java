39f8a3039cd3c80c8f87a15eda8bcf9b83f875b6
pyxixi2012 <yangpengdi1000@gmail.com>
2016-03-08 18:08:37 +0800

    recreate project

---
+++ src/main/java/com/zgkdata/ZgkdataApplication.java
@@ -0,0 +1,12 @@
+package com.zgkdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZgkdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZgkdataApplication.class, args);
+	}
+}
