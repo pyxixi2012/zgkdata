498cf006b220a76581fe6c0cecea84a3d4c62208
pyxixi2012 <yangpengdi1000@gmail.com>
2016-03-08 17:42:19 +0800

    commit add controller file

---
+++ src/main/java/com/zgg/Widget.java
@@ -0,0 +1,32 @@
+package com.zgg;
+
+/**
+ * Created by Administrator on 2016/3/8.
+ */
+
+public class Widget {
+
+    private String type;
+
+    private int length;
+
+    private int height;
+
+    public Widget(String type, int length, int height) {
+        this.type = type;
+        this.length = length;
+        this.height = height;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
